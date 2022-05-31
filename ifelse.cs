39cb9f11142f270ea78bed1038ad27b0f3d30592
SasikalaIyyamperumal <Sasikala@techeva.co.in>
2022-05-31 12:48:14 +0530

    ifelse.cs

---
+++ ifelse.cs
@@ -0,0 +1,16 @@
+int x = 90, y = 80;
+ 
+if (x > y)
+{
+  Console.WriteLine("x is greater than y");
+} 
+else if (x < y) 
+{
+  Console.WriteLine("x is less than y");
+} 
+else
+{
+  Console.WriteLine("x is equal to y");
+}
+ 
+© 2022 GitHu
